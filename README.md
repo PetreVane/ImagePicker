a7d04f1caa91b59334a87370332cfe6566cab72a
Petre Vane <PetreVane@users.noreply.github.com>
2020-06-19 16:55:51 +0200

    Added ImagePicker and updated ReadMe file

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # ImagePicker
 
-A description of this package.
+This package contains an ImagePicker. 
+
+Usage:
+
+1. your viewController should have a reference of type ImagePicker
+    
+        var imagePicker: ImagePicker?
+        
+2. make your viewController conform to ImagePicker delegate
+
+    
+        // MARK: - Adopts ImagePicker delegate
+        extension ChatViewController: ImagePickerDelegate {
+            func didSelect(image: UIImage) {
+                let message = Message(image: image, user: currentlyLoggedInUser!, messageID: UUID().uuidString, date: Date())
+                cloudFirestore.upload(message: message, from: chatRoom!)
+                print("Image has been selected and upload triggered")
+            }
+        }
+        
+        // MARK: - Sets itself as ImagePicker delegate
+            private func configureImagePicker() {
+                   imagePicker = ImagePicker(presentingController: self, delegate: self)
+            }
+        
+
--- Sources/ImagePicker/ImagePicker.swift
+++ Sources/ImagePicker/ImagePicker.swift
@@ -1,3 +1,105 @@
-struct ImagePicker {
-    var text = "Hello, World!"
+
+import UIKit
+import AVFoundation
+
+protocol ImagePickerDelegate: AnyObject {
+    func didSelect(image: UIImage)
 }
+
+class ImagePicker: NSObject {
+    
+    var imagePickerController: UIImagePickerController
+    weak var presentingController: UIViewController?
+    weak var delegate: ImagePickerDelegate?
+    
+    init(presentingController: UIViewController, delegate: ImagePickerDelegate) {
+        self.imagePickerController = UIImagePickerController()
+        super.init()
+        self.presentingController = presentingController
+        self.delegate = delegate
+        self.imagePickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        self.imagePickerController.allowsEditing = true
+        self.imagePickerController.mediaTypes = ["public.image"]
+    }
+    
+    func selectImageFrom(source type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(type) else { return nil }
+        return UIAlertAction(title: title, style: .default) { [unowned self] (action) in
+            
+            self.imagePickerController.sourceType = type
+            self.presentingController?.present(self.imagePickerController, animated: true, completion: nil)
+            
+        }
+    }
+    
+    func presentAlert(from view: UIView) {
+        
+        let alertController = UIAlertController(title: "Pick your Image", message: "Select where your image is saved", preferredStyle: .alert)
+        
+        if let cameraAction = self.selectImageFrom(source: .camera, title: "Camera") {
+            
+            if allowedCameraAccess() {
+                alertController.addAction(cameraAction)
+            }
+        }
+        
+        if let photoLibraryAction = self.selectImageFrom(source: .photoLibrary, title: "Photo Library") {
+            alertController.addAction(photoLibraryAction)
+        }
+        
+        if let savedPhotosAction = self.selectImageFrom(source: .savedPhotosAlbum, title: "Saved Photos") {
+            alertController.addAction(savedPhotosAction)
+        }
+        
+        self.presentingController?.present(alertController, animated: true, completion: nil)
+    }
+    
+    func imagePicker(didSelect image: UIImage?, _ controller: UIImagePickerController) {
+        
+        if let selectedImage = image {
+            self.delegate?.didSelect(image: selectedImage)
+        }
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func allowedCameraAccess() -> Bool {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        // The user has previously granted access to the camera.
+        case .authorized: return true
+        // The user has not yet been asked for camera access.
+        case .notDetermined:
+            var accessPermited: Bool = false
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted { accessPermited = true }
+            }
+            return accessPermited
+        // The user has previously denied access.
+        case .denied: return false
+        // The user can't grant access due to restrictions.
+        case .restricted: return false
+        @unknown default:
+            print("Unknown case added")
+            return false
+        }
+    }
+}
+
+extension ImagePicker: UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.imagePicker(didSelect: nil, picker)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return self.imagePicker(didSelect: nil, picker) }
+        self.imagePicker(didSelect: image, picker)
+    }
+    
+}
+
+
+extension ImagePicker: UINavigationControllerDelegate { }
+
